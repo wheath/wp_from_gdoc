1bf3c5b67a80f59c89ef121df0f71874ac85d141
Tim Heath <wheath@superb.net>
2014-12-23 19:06:50 -1000

    adding after title info gathering

--- wp_gen_from_gdoc.py
+++ wp_gen_from_gdoc.py
@@ -41,10 +41,16 @@ pages = []
 from BeautifulSoup import BeautifulSoup
 import mechanize
 
-def get_title(html_lines, cur_line_num):  
+def get_header_info(html_lines, cur_line_num):  
+
+  print '_dbg in get_header_info'
+  print '_dbg cur_line_num 1: %d' % cur_line_num
+  cur_line = html_lines[cur_line_num]
+  print '_dbg initial cur_line: %s' % cur_line
   title = ''
   cur_line_num +=1
   cur_line = html_lines[cur_line_num]
+  print '_dbg cur_line: %s' % cur_line
   if 'name' in cur_line:
     cur_line_num = cur_line_num+2
     cur_line = html_lines[cur_line_num]
@@ -53,15 +59,72 @@ def get_title(html_lines, cur_line_num):
       title = html_lines[cur_line_num]  
       return title, cur_line_num+2
 
+  return '', cur_line_num
+
+def get_raw_info(html_lines, cur_line_num, tag_name):  
+  end_tag = "</%s>" % tag_name
+  while cur_line_num < len(html_lines) and end_tag  not in cur_line:
+    raw_str = html_lines[cur_line_num]
+    cur_line_num += 1
+
+  return raw_str, cur_line_num
+    
+  
+
+def get_title(html_lines, cur_line_num):  
+  print '_dbg in get_title'
+  print '_dbg cur_line_num 2: %d' % cur_line_num
+  return get_header_info(html_lines, cur_line_num)
+
+def get_tag(html_lines, cur_line_num, after_title, tag_name):
+  if 'h' in tag_name:
+    print '_dbg about to call get_header_info'
+    print '_dbg cur_line_num 3: %d' % cur_line_num
+    tag_str, cur_line_num = get_header_info(html_lines, cur_line_num)
+  else:
+    tag_str, cur_line_num = get_raw_info(html_lines, cur_line_num, tag_name)
+
+  if tag_str:
+    new_tag = {}
+    new_tag[tag_name] = tag_str
+    after_title.append(new_tag)      
+
+  return cur_line_num
+  
+
+def get_after_title(html_lines, cur_line_num, after_title):
+  print 'in get_after_title' 
+  cur_line = html_lines[cur_line_num]
+  print '_dbg cur_line: %s' % cur_line
+  while cur_line_num < len(html_lines) and ' title' not in cur_line:
+    print '_dbg checking cur_line: %s' % cur_line
+    if '<h1' in cur_line:
+      print "_dbg about to call get_tag"
+      print '_dbg cur_line_num 4: %d' % cur_line_num
+      cur_line_num = get_tag(html_lines, cur_line_num, after_title, 'h1')
+    cur_line_num += 1
+    cur_line = html_lines[cur_line_num]
+
+  if ' title' in cur_line:
+    cur_line_num += -1
+
+  return cur_line_num
+
 
 def get_page_info(html_lines, cur_line_num):  
   page_info = {}
-  try:
-    title, cur_line_num = get_title(html_lines, cur_line_num)  
-    if title:
-      page_info['title'] = title
-  finally:
-    return page_info, cur_line_num
+  #try:
+  title, cur_line_num = get_title(html_lines, cur_line_num)  
+  if title:
+    page_info['title'] = title
+    page_info['after_title'] = []
+    after_title = page_info['after_title']
+    print '_dbg about to call get_after_title'
+    if cur_line_num + 1 < len(html_lines):
+      print '_dbg cur_line_num 5: %d' % cur_line_num 
+      cur_line_num = get_after_title(html_lines, cur_line_num+1, after_title)  
+  #finally:
+  return page_info, cur_line_num
 
 def get_gdoc_as_html():
 
@@ -113,13 +176,13 @@ if found:
       cur_line_num += 1
       continue
     
-    try:
-      if ' title' in cur_line in cur_line: 
-	page_info, cur_line_num = get_page_info(html_lines, cur_line_num)  
-	if page_info:
-	  pages.append(page_info)
-    finally:
-      cur_line_num += 1
+    #try:
+    if ' title' in cur_line in cur_line: 
+      page_info, cur_line_num = get_page_info(html_lines, cur_line_num)  
+      if page_info:
+	pages.append(page_info)
+    #finally:
+    cur_line_num += 1
 
 print "_dbg num pages found: %d" % len(pages)
 for page in pages:
